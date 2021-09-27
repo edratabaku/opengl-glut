f86be32eacabdf8475fcf59a3429ff8711271eb6
Edra Tabaku <68347994+edratabaku@users.noreply.github.com>
2021-09-27 17:50:01 +0200

    Added code commentation

--- nonContinousLines.cpp
+++ nonContinousLines.cpp
@@ -2,19 +2,24 @@
 #include <GL/glut.h>
 
 int i=0;
+//structure that holds the coordinates of a points
 struct pika {
 	int xp;
 	int yp;
 };
+//array of points
 struct pika pikat[100];
+//initializer for the viewport
 void init(){
 	glClearColor(0.0,0.0,0.0,0.0);
 	glViewport(0,0,300,300);
 	gluOrtho2D(0.0,300.0,0.0,300.0);
 }
+//clear the buffer
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 }
+//draws a line that connects two points
 void drawLine(pika pt1, pika pt2){
 	glBegin(GL_LINE_STRIP);
 	glVertex2i(pt1.xp,pt1.yp);
@@ -22,6 +27,9 @@ void drawLine(pika pt1, pika pt2){
 	glEnd();
 	glFlush();
 }
+//function that handles mouse events
+//when the number of clicks is even, it saves the coordinates of the last click.
+//when the number of clicks is odd, it draws a line that connects the last two clicks.
 void mouse(int button, int state, int x, int y){
 	if (button==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
 		if(i%2==0){
@@ -38,6 +46,7 @@ void mouse(int button, int state, int x, int y){
 	}
 	glutPostRedisplay();
 }
+//main function
 int main(int argc, char*argv[]){
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
